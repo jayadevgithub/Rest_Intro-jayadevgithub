49f0bfc33bf7e136d58b7c9eb86efeb815205289
Jayadev Peddibhotla <jayadev.peddibhotla@kony.com>
2015-09-07 18:21:49 +0700

    commit changes

--- src/CalculatorHttpServer.js
+++ src/CalculatorHttpServer.js
@@ -14,8 +14,59 @@
 */
 
 var http = require('http');
+var url = require('url');
 var querystring = require('querystring');
 var PORT = 3000;
+var req_url = "/calculator/sum";
 
+function validateURL(url) {
+    if(url.substring(0,url.lastIndexOf("/")+4) != req_url){
+        return false;
+    }
+    return true;
+}
+
+function getQueryParams(request, queryParams, url, bodyStr, response) {
+    if (request.method == 'GET') {
+        queryParams = querystring.parse(url.substring(url.indexOf("?") + 1));
+    } else if (request.method == 'POST') {
+        queryParams = JSON.parse(bodyStr);
+    } else {
+        response.end("Unsupported request method : " + request.method);
+    }
+    return queryParams;
+}
+
+function handleRequest(request, response){
+    var bodyStr = '';
+
+    request.on('data', function (chunk) {
+        bodyStr += chunk.toString();
+    });
+
+    request.on('end', function() {
+        var queryParams;
+        var url = request.url;
+        var isUrlValid = validateURL(url);
+        if(isUrlValid){
+            queryParams = getQueryParams(request, queryParams, url, bodyStr, response);
+            var sum = parseInt(queryParams.op1) + parseInt(queryParams.op2);
+            if(isNaN(sum)){
+                response.statusCode = 400;
+                response.end("Bad Request");
+            }
+            response.end(sum.toString());
+        }else{
+            response.statusCode = 404;
+            response.end("Invalid URL");
+        }
+    });
+}
 // Add your code to startup http server and process request here.
+var server = http.createServer(handleRequest);
+
+server.listen(PORT, function(){
+   console.log("server listening on port: "+PORT);
+});
+
 
